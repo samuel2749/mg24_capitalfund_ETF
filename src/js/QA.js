fcc7e4e7220fe85b8a2ebf461607ffbf3f75fb3d
Fergus <fergus.jingtai@gmail.com>
2017-03-17 19:42:39 +0800

    update Qa.html

--- src/js/QA.js
+++ src/js/QA.js
@@ -1,10 +1,25 @@
 $(function() {
+  var accordionTitleLi = $('.accordion-title li');
+  var accordionList = $('.accordion-list');
+
+  // accordionTitle click
+  accordionTitleLi.on('click', function() {
+    var j_this = $(this);
+    var index = j_this.index();
+    if (j_this.hasClass('active')) return;
+
+    j_this.addClass('active').siblings().removeClass('active');
+    accordionList.hide().eq(index).show();
+    // accordionList.css({position: 'absolute'}).fadeOut().eq(index).css({position: 'relative'}).fadeIn();
+  });
+
+  // accordionList click
   $('.list-title').click(function() {
 		$('.list-sub').slideUp();
-		$('.list-btn').css('background', 'url(images/award/list-open-btn.jpg)');
+		$('.switch-icon').css('backgroundPositionY', 0);
 		if ( $(this).next().css('display') == 'none' ) {
 			$(this).next().slideDown();
-			$(this).find('.list-btn').css('background', 'url(images/award/list-close-btn.jpg)');
+			$(this).find('.switch-icon').css('backgroundPositionY', -15);
 		}
 	});
 });
