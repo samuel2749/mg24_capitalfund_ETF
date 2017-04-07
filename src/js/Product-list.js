752fddd176b1c5df93db8add35f5486a5db02484
fergus <fergus.jingtai@gmail.com>
2017-04-07 10:03:36 +0800

    update

--- src/js/Product-list.js
+++ src/js/Product-list.js
@@ -1,6 +1,6 @@
 $(function() {
   var accordionTitleLi = $('.accordion-title li');
-  var accordionList = $('.other .table-box');
+  var accordionList = $('.all-box > div');
 
   // accordionTitle click
   accordionTitleLi.on('click', function() {
@@ -14,37 +14,5 @@ $(function() {
     // accordionList.css({position: 'absolute'}).fadeOut().eq(index).css({position: 'relative'}).fadeIn();
   });
 
-
-  datepicker();
-
-  function datepicker() {
-    $( "#datepicker" ).datepicker({
-      dateFormat: "yy-mm-dd",
-      onSelect: function() {
-        var currentDate = $( "#datepicker" ).datepicker( "getDate" );
-        console.log($('#datepicker').val());
-        var val = $('#datepicker').val()
-        $('.info-date span').html(val);
-        console.log('currentDate = ', currentDate.getFullYear()  );
-      }
-    });
-    // $( "#datepicker" ).datepicker();
-    //$( "#datepicker" ).datepicker( "option", "dateFormat", "yy-mm-dd" );
-
-
-    // $("#datepicker").datepicker({
-    //   dateFormat: "yy-mm-dd",
-    //   onSelect: function() {
-    //   var currentDate = $( "#datepicker" ).datepicker( "getDate" );
-    //   console.log('currentDate = ', currentDate);
-    // }});
-
-    $('.select-date').on('click', function() {
-      $( "#datepicker" ).datepicker( "show" );
-    });
-    //
-
-
-  }
 
 });
