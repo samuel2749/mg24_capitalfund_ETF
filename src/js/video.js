6f9e50be7969e6b683c68f26f118b8578a88d2cf
Fergus <fergus.jingtai@gmail.com>
2017-04-07 17:54:28 +0800

    update

---
+++ src/js/video.js
@@ -0,0 +1,18 @@
+$(function() {
+  var accordionTitleLi = $('.accordion-title li');
+  var accordionList = $('.all-box > div');
+
+  // accordionTitle click
+  accordionTitleLi.on('click', function() {
+    var j_this = $(this);
+    var index = j_this.index();
+    if (j_this.hasClass('active')) return;
+
+    j_this.addClass('active').siblings().removeClass('active');
+    console.log(index);
+    accordionList.hide().eq(index).show();
+    // accordionList.css({position: 'absolute'}).fadeOut().eq(index).css({position: 'relative'}).fadeIn();
+  });
+
+
+});
